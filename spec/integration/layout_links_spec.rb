db27f03dfc369abde177655f3a49f906649135fc
Shariq Mobin <scarshark@gmail.com>
2010-03-25 15:28:28 -0700

    Annotated models

--- spec/integration/layout_links_spec.rb
+++ spec/integration/layout_links_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
+require "webrat"
 
 describe "LayoutLinks" do
+
+  Webrat.configure do |config|
+    config.mode = :rails
+  end
+
   it "should have a Home page at '/'" do
     get '/'
     response.should render_template('pages/home')
@@ -27,5 +33,16 @@ describe "LayoutLinks" do
     response.should render_template('users/new')
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should render_template('pages/about')
+    click_link "Help"
+    response.should render_template('pages/help')
+    click_link "Contact"
+    response.should render_template('pages/contact')
+    click_link "Home"
+    response.should render_template('pages/home')
+  end
 end
 
